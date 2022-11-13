67b8ac3e4581eda4db47fd48bdf2582d48614b66
USER <yarmiztechnlogies@gmail.com>
2022-11-13 16:55:06 +0530

    <MounikEnzapps>

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2016-2018 Akretion (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice UBL Inherit",
+    "version": "14.0.1.0.6",
+    "category": "Accounting & Finance",
+    "license": "AGPL-3",
+    "summary": "Generate UBL XML file for customer invoices/refunds",
+    "author": "Akretion,Onestein,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi/",
+    "depends": [
+      "account_invoice_ubl",
+    ],
+    "data": [
+             ],
+    "installable": True,
+}
